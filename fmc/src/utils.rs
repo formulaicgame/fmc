89e1f78c6aafc94812a3a6b8f4917d340c28405a
fmcgg <github@fmc.gg>
2025-03-26 21:52:22 +0100

    Random numbers from ranges

--- fmc/src/utils.rs
+++ fmc/src/utils.rs
@@ -37,6 +37,8 @@
 //     return (chunk_pos, block_index);
 // }
 
+use std::ops::{Range, RangeInclusive};
+
 #[derive(Default, Debug, Clone)]
 pub struct Rng {
     seed: u64,
@@ -63,5 +65,58 @@ impl Rng {
         // with exponent of 127 so the result is in range 1..2 then -1 to move the range down
         // to 0..1
         f32::from_bits((result >> 9) | (127 << 23)) - 1.0
+    }
+
+    pub fn range_u32(&mut self, range: impl RngRange<u32>) -> u32 {
+        range.start() + self.next_u32() % (range.end() - range.start() + 1)
+    }
+
+    pub fn range_f32(&mut self, range: impl RngRange<f32>) -> f32 {
+        range.start() + self.next_f32() * (range.end() - range.start())
+    }
+}
+
+pub trait RngRange<T> {
+    fn start(&self) -> T;
+    fn end(&self) -> T;
+}
+
+impl RngRange<f32> for Range<f32> {
+    fn start(&self) -> f32 {
+        self.start
+    }
+
+    fn end(&self) -> f32 {
+        self.end
+    }
+}
+
+impl RngRange<f32> for RangeInclusive<f32> {
+    fn start(&self) -> f32 {
+        *self.start()
+    }
+
+    fn end(&self) -> f32 {
+        *self.end()
+    }
+}
+
+impl RngRange<u32> for Range<u32> {
+    fn start(&self) -> u32 {
+        self.start
+    }
+
+    fn end(&self) -> u32 {
+        self.end - 1
+    }
+}
+
+impl RngRange<u32> for RangeInclusive<u32> {
+    fn start(&self) -> u32 {
+        *self.start()
+    }
+
+    fn end(&self) -> u32 {
+        *self.end()
     }
 }
